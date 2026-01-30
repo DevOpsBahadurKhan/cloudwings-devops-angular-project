3a55a82908311f08028bf7de9eb87431652f4361
Bahadur <bk209203@gmail.com>
2026-01-30 11:51:20 +0530

    done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://10.57.88.165:3000'
+  apiUrl: '/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
+//C:\Users\HP\Desktop\Office Project\blogger-frontend\facebook\src\environments\environment.ts
 export const environment = {
   production: false,
   apiUrl: 'http://10.57.88.165:3000'
